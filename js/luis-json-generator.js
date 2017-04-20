3a4db84297620784e96f4a5927a47f5f018c90e9
CLachaise <cedric.lachaise@cegedim.fr>
2017-04-20 22:05:37 +0200

    Added support for LUIS API v1 & v2

--- js/luis-json-generator.js
+++ js/luis-json-generator.js
@@ -1,4 +1,4 @@
-function luisJSONGenerator(inputData, entities)
+function luisJSONGenerator(inputData, entities, version)
 {
   var keys = Object.keys(inputData);
   var utterances = {};
@@ -30,16 +30,35 @@ function luisJSONGenerator(inputData, entities)
 
         if(utt.indexOf(key) > -1){
           entityLabel = {};
-          entityLabel["entityName"] = key.toString().substring(1, key.toString().length - 1);
-          entityLabel["startCharIndex"] = utt.indexOf(key);
-          entityLabel["endCharIndex"] = entityLabel["startCharIndex"] + value.length - 1;
+
+          switch(version){
+            case "1" :
+              entityLabel["entity"] = key.toString().substring(1, key.toString().length - 1);
+              entityLabel["startPos"] = utt.indexOf(key);
+              entityLabel["endPos"] = entityLabel["startPos"] + value.length - 1;
+            break;
+            case "2" :
+            entityLabel["entityName"] = key.toString().substring(1, key.toString().length - 1);
+            entityLabel["startCharIndex"] = utt.indexOf(key);
+            entityLabel["endCharIndex"] = entityLabel["startCharIndex"] + value.length - 1;
+            break;
+          }
           entityLabels.push(entityLabel);
         }
       })
 
+      switch(version){
+        case "1" :
+          block["intent"] = key.toString();
+          block["entities"] = entityLabels;
+        break;
+        case "2" :
+          block["intentName"] = key.toString();
+          block["entityLabels"] = entityLabels;
+        break;
+      }
+
       block["text"] = newLine;
-      block["intentName"] = key.toString();
-      block["entityLabels"] = entityLabels;
 
       if(newLine != "")
         LUISBody.push(block);
